carryon/tendermint

state: persist validators

state/state_test.go
@@ -1,18 +1,21 @@
 package state
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
 	abci "github.com/tendermint/abci/types"
 	crypto "github.com/tendermint/go-crypto"
-	cfg "github.com/tendermint/tendermint/config"
+	cmn "github.com/tendermint/tmlibs/common"
 	dbm "github.com/tendermint/tmlibs/db"
 	"github.com/tendermint/tmlibs/log"
+
+	cfg "github.com/tendermint/tendermint/config"
 )
 
 func TestStateCopyEquals(t *testing.T) {
+	assert := assert.New(t)
 	config := cfg.ResetTestRoot("state_")
 
 	// Get State db
@@ -22,18 +25,13 @@ func TestStateCopyEquals(t *testing.T) {
 
 	stateCopy := state.Copy()
 
-	if !state.Equals(stateCopy) {
-		t.Fatal("expected state and its copy to be identical. got %v\n expected %v\n", stateCopy, state)
-	}
-
+	assert.True(state.Equals(stateCopy), cmn.Fmt("expected state and its copy to be identical. got %v\n expected %v\n", stateCopy, state))
 	stateCopy.LastBlockHeight += 1
-
-	if state.Equals(stateCopy) {
-		t.Fatal("expected states to be different. got same %v", state)
-	}
+	assert.False(state.Equals(stateCopy), cmn.Fmt("expected states to be different. got same %v", state))
 }
 
 func TestStateSaveLoad(t *testing.T) {
+	assert := assert.New(t)
 	config := cfg.ResetTestRoot("state_")
 	// Get State db
 	stateDB := dbm.NewDB("state", config.DBBackend, config.DBDir())
@@ -44,9 +42,7 @@ func TestStateSaveLoad(t *testing.T) {
 	state.Save()
 
 	loadedState := LoadState(stateDB)
-	if !state.Equals(loadedState) {
-		t.Fatal("expected state and its copy to be identical. got %v\n expected %v\n", loadedState, state)
-	}
+	assert.True(state.Equals(loadedState), cmn.Fmt("expected state and its copy to be identical. got %v\n expected %v\n", loadedState, state))
 }
 
 func TestABCIResponsesSaveLoad(t *testing.T) {
@@ -74,5 +70,30 @@ func TestABCIResponsesSaveLoad(t *testing.T) {
 
 	state.SaveABCIResponses(abciResponses)
 	abciResponses2 := state.LoadABCIResponses()
-	assert.Equal(abciResponses, abciResponses2, fmt.Sprintf("ABCIResponses don't match: Got %v, Expected %v", abciResponses2, abciResponses))
+	assert.Equal(abciResponses, abciResponses2, cmn.Fmt("ABCIResponses don't match: Got %v, Expected %v", abciResponses2, abciResponses))
+}
+
+func TestValidatorsSaveLoad(t *testing.T) {
+	assert := assert.New(t)
+	config := cfg.ResetTestRoot("state_")
+	// Get State db
+	stateDB := dbm.NewDB("state", config.DBBackend, config.DBDir())
+	state := GetState(stateDB, config.GenesisFile())
+	state.SetLogger(log.TestingLogger())
+
+	state.LastBlockHeight += 1
+
+	state.SaveValidators()
+	v, err := state.LoadValidators(state.LastBlockHeight)
+	assert.Nil(err, "expected no err")
+	assert.Equal(v.Hash(), state.Validators.Hash(), "expected validator hashes to match")
+
+	state.LastBlockHeight += 10
+	state.SaveValidators()
+	v, err = state.LoadValidators(state.LastBlockHeight)
+	assert.Nil(err, "expected no err")
+	assert.Equal(v.Hash(), state.Validators.Hash(), "expected validator hashes to match")
+
+	_, err = state.LoadValidators(state.LastBlockHeight + 1)
+	assert.NotNil(err, "expected err")
 }

celestia-org/celestia-core

state/state_test.go
@@ -1,18 +1,21 @@
 package state
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
 	abci "github.com/tendermint/abci/types"
 	crypto "github.com/tendermint/go-crypto"
-	cfg "github.com/tendermint/tendermint/config"
+	cmn "github.com/tendermint/tmlibs/common"
 	dbm "github.com/tendermint/tmlibs/db"
 	"github.com/tendermint/tmlibs/log"
+
+	cfg "github.com/tendermint/tendermint/config"
 )
 
 func TestStateCopyEquals(t *testing.T) {
+	assert := assert.New(t)
 	config := cfg.ResetTestRoot("state_")
 
 	// Get State db
@@ -22,18 +25,13 @@ func TestStateCopyEquals(t *testing.T) {
 
 	stateCopy := state.Copy()
 
-	if !state.Equals(stateCopy) {
-		t.Fatal("expected state and its copy to be identical. got %v\n expected %v\n", stateCopy, state)
-	}
-
+	assert.True(state.Equals(stateCopy), cmn.Fmt("expected state and its copy to be identical. got %v\n expected %v\n", stateCopy, state))
 	stateCopy.LastBlockHeight += 1
-
-	if state.Equals(stateCopy) {
-		t.Fatal("expected states to be different. got same %v", state)
-	}
+	assert.False(state.Equals(stateCopy), cmn.Fmt("expected states to be different. got same %v", state))
 }
 
 func TestStateSaveLoad(t *testing.T) {
+	assert := assert.New(t)
 	config := cfg.ResetTestRoot("state_")
 	// Get State db
 	stateDB := dbm.NewDB("state", config.DBBackend, config.DBDir())
@@ -44,9 +42,7 @@ func TestStateSaveLoad(t *testing.T) {
 	state.Save()
 
 	loadedState := LoadState(stateDB)
-	if !state.Equals(loadedState) {
-		t.Fatal("expected state and its copy to be identical. got %v\n expected %v\n", loadedState, state)
-	}
+	assert.True(state.Equals(loadedState), cmn.Fmt("expected state and its copy to be identical. got %v\n expected %v\n", loadedState, state))
 }
 
 func TestABCIResponsesSaveLoad(t *testing.T) {
@@ -74,5 +70,41 @@ func TestABCIResponsesSaveLoad(t *testing.T) {
 
 	state.SaveABCIResponses(abciResponses)
 	abciResponses2 := state.LoadABCIResponses()
-	assert.Equal(abciResponses, abciResponses2, fmt.Sprintf("ABCIResponses don't match: Got %v, Expected %v", abciResponses2, abciResponses))
+	assert.Equal(abciResponses, abciResponses2, cmn.Fmt("ABCIResponses don't match: Got %v, Expected %v", abciResponses2, abciResponses))
+}
+
+func TestValidatorsSaveLoad(t *testing.T) {
+	assert := assert.New(t)
+	config := cfg.ResetTestRoot("state_")
+	// Get State db
+	stateDB := dbm.NewDB("state", config.DBBackend, config.DBDir())
+	state := GetState(stateDB, config.GenesisFile())
+	state.SetLogger(log.TestingLogger())
+
+	// cant load anything for height 0
+	v, err := state.LoadValidators(0)
+	assert.NotNil(err, "expected err at height 0")
+
+	// should be able to load for height 1
+	v, err = state.LoadValidators(1)
+	assert.Nil(err, "expected no err at height 1")
+	assert.Equal(v.Hash(), state.Validators.Hash(), "expected validator hashes to match")
+
+	// increment height, save; should be able to load for next height
+	state.LastBlockHeight += 1
+	state.SaveValidators()
+	v, err = state.LoadValidators(state.LastBlockHeight + 1)
+	assert.Nil(err, "expected no err")
+	assert.Equal(v.Hash(), state.Validators.Hash(), "expected validator hashes to match")
+
+	// increment height, save; should be able to load for next height
+	state.LastBlockHeight += 10
+	state.SaveValidators()
+	v, err = state.LoadValidators(state.LastBlockHeight + 1)
+	assert.Nil(err, "expected no err")
+	assert.Equal(v.Hash(), state.Validators.Hash(), "expected validator hashes to match")
+
+	// should be able to load for next next height
+	_, err = state.LoadValidators(state.LastBlockHeight + 2)
+	assert.NotNil(err, "expected err")
 }